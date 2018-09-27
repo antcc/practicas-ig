82380979abfe9986d07cb1d720ea873911d6deba
antcc <antoniocoin07@gmail.com>
2018-09-27 17:35:03 +0200

    Añade archivos iniciales P2

---
+++ alum-srcs/practica2.hpp
@@ -0,0 +1,15 @@
+// *********************************************************************
+// **
+// ** Informática Gráfica, curso 2016-17
+// ** Práctica 2  (declaraciones públicas)
+// **
+// *********************************************************************
+
+#ifndef IG_PRACTICA2_HPP
+#define IG_PRACTICA2_HPP
+
+void P2_Inicializar(  ) ;
+bool P2_FGE_PulsarTeclaCaracter(  unsigned char tecla ) ;
+void P2_DibujarObjetos( ContextoVis & cv ) ;
+
+#endif
